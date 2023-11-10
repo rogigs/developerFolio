b996718390bf13f2a82112ec43c3b01976c432bf
Igor Soares <igorgomes.13ig@gmail.com>
2023-11-10 12:54:10 -0300

    feat: add section open source

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -20,6 +20,7 @@ import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
 import {useLocalStorage} from "../hooks/useLocalStorage";
 import "./Main.scss";
+import OpenSource from "./openSource/OpenSource";
 
 const Main = () => {
   const darkPref = window.matchMedia("(prefers-color-scheme: dark)");
@@ -57,6 +58,7 @@ const Main = () => {
             <Education />
             <WorkExperience />
             <Projects />
+            <OpenSource />
             <StartupProject />
             <Achievement />
             <Blogs />
